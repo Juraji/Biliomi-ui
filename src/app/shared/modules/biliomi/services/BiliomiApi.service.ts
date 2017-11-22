37edfbcd6b80a22e9b9622b3e8241f8098bf8eaa
rkesseler <r.kesseler@convect.nl>
2017-11-22 09:07:39 +0100

    - BiliomiApi.service.ts: Add descriptions to public methods, explaining usage

--- src/app/shared/modules/biliomi/services/BiliomiApi.service.ts
+++ src/app/shared/modules/biliomi/services/BiliomiApi.service.ts
@@ -22,10 +22,13 @@ export class BiliomiApiService {
     this._postRequestErrorInterceptor = new EventEmitter<HttpErrorResponse>();
   }
 
-  public get postRequestErrorInterceptor(): EventEmitter<HttpErrorResponse> {
-    return this._postRequestErrorInterceptor;
-  }
-
+  /**
+   * Perform a GET request on Biliomi's Api to retrieve (lists of) entities.
+   * @param {string} resourceUri The entity resource uri, without api prefix. E.g. "/core/games" or "/core/games/1".
+   * @param {HttpParams} params Any query parameters to send with the request.
+   * @return {Promise<T>} A promise to the result.
+   * The promise could resolve with NULL of there was no content or an error occurred.
+   */
   public async get<T>(resourceUri: string, params?: HttpParams): Promise<T> {
     let uri: string = await this.getApiUriFor(resourceUri);
 
@@ -43,6 +46,14 @@ export class BiliomiApiService {
     return null;
   }
 
+  /**
+   * Perform a POST request on Biliomi's Api in order to create new entities or perform actions.
+   * @param {string} resourceUri The resource uri, without api prefix. E.g. "/auth/login".
+   * @param {T} body The object representing the body of the request.
+   * @param {HttpParams} params Any query parameters to send with the request.
+   * @return {Promise<R>} A promise to the result.
+   * The promise could resolve with NULL of there was no content or an error occurred.
+   */
   public async post<T, R>(resourceUri: string, body: T, params?: HttpParams): Promise<R> {
     let uri: string = await this.getApiUriFor(resourceUri);
 
@@ -60,6 +71,14 @@ export class BiliomiApiService {
     return null;
   }
 
+  /**
+   * Perform a PUT request on Biliomi's Api in order to update entities.
+   * @param {string} resourceUri The resource uri of the target entity, without api prefix. E.g. "/core/games/1".
+   * @param body The object representing the updated entity
+   * @param {HttpParams} params Any query parameters to send with the request.
+   * @return {Promise<T>} A promise to the persisted entity.
+   * The promise could resolve with NULL of there was no change comitted or an error occurred.
+   */
   public async put<T>(resourceUri: string, body: any, params?: HttpParams): Promise<T> {
     let uri: string = await this.getApiUriFor(resourceUri);
 
@@ -78,6 +97,12 @@ export class BiliomiApiService {
   }
 
   // noinspection ReservedWordAsName
+  /**
+   * Perform a DELETE request on Biliomi's Api in order to delete entities.
+   * @param {string} resourceUri The resource uri of the target entity, without api prefix. E.g. "/core/games/1".
+   * @param {HttpParams} params params Any query parameters to send with the request.
+   * @return {Promise<boolean>} A promise to the success state, being True when successful and False when failed.
+   */
   public async delete(resourceUri: string, params?: HttpParams): Promise<boolean> {
     let uri: string = await this.getApiUriFor(resourceUri);
     let response: HttpResponse<void>;
@@ -97,6 +122,13 @@ export class BiliomiApiService {
     return response != null && response.status == 200;
   }
 
+  /**
+   * Have Biliomi execute a command as if it were posted in the chat.
+   * @param {string} command The command to execute.
+   * @param args Any parameters to apply with the command
+   * @return {Promise<boolean>} A promise to the success state of the request, being True when successful and False when failed.
+   * The promise resulting in true does NOT guarantee a successful command execution!
+   */
   public async postCommand(command: string, ...args: any[]): Promise<boolean> {
     let req: ICommandRequest = {Command: command + " " + args.join(" ")};
     let uri: string = await this.getApiUriFor(BILIOMI_API.COMMAND_ENDPOINT);
@@ -116,6 +148,13 @@ export class BiliomiApiService {
     return response != null && response.status == 200;
   }
 
+  /**
+   * Have Biliomi execute a cli command as if it were posted in the console.
+   * @param {string} command The command to execute.
+   * @param args Any parameters to apply with the command
+   * @return {Promise<boolean>} A promise to the success state of the request, being True when successful and False when failed.
+   * The promise resulting in true does NOT guarantee a successful command execution!
+   */
   public async postCliCommand(command: string, ...args: any[]): Promise<boolean> {
     let req: ICommandRequest = {Command: command + " " + args.join(" ")};
     let uri: string = await this.getApiUriFor(BILIOMI_API.CLI_COMMAND_ENDPOINT);
@@ -133,6 +172,15 @@ export class BiliomiApiService {
     }
 
     return response != null && response.status == 200;
+  }
+
+  /**
+   * Subscribe to any error event that occurs while comunicating with Biliomi.
+   * Do note that a subscriber of this emitter will get executed with every and all errors.
+   * @return {EventEmitter<HttpErrorResponse>}
+   */
+  public get postRequestErrorInterceptor(): EventEmitter<HttpErrorResponse> {
+    return this._postRequestErrorInterceptor;
   }
 
   private async getApiUriFor(resourceUri: string): Promise<string> {
